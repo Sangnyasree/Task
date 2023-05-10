0977cd220a355078c6ff597777fb7873bef6930f
Sangnyasree Biswalo <b-sangnyasree@inahosolutions.com>
2023-05-10 11:27:05 +0530

    add one method in 425th line

--- src/test/java/com/base/BaseClass.java
+++ src/test/java/com/base/BaseClass.java
@@ -422,5 +422,8 @@ return firstSelectedOption;
     	js.executeScript("arguments[0].scrollIntoView(true)",element);
     	
     }
-
+    public void clear2(WebElement element) {
+		elementVisibility(element);
+		element.clear();
+	}
 }
